efbebd3a96e027259b5a491906ad67e82ef7cfa8
santilife <santiagootalvaro678@gmail.com>
2025-02-28 08:32:19 -0500

    Commit

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_mysqldb import MySQL
 
 
 app=Flask(__name__)
+
+
+
+# ---------------------------------------------------------------------------- #
+#                                  Peticiones                                  #
+# ---------------------------------------------------------------------------- #
+@app.before_request
+def before_request():
+    print("Antes de la petición...")
+
+   
+@app.after_request
+def after_request(response):
+    print("Después de la petición")
+    return response  
+
 
 
 # ---------------------------------------------------------------------------- #
@@ -16,12 +32,32 @@ app.config['MYSQL_DB'] = 'Kliiker'
 conexion = MySQL(app)
 
 
+@app.route('/consulta')
+def listar_datos():
+    data={}
+    try:
+        cursor=conexion.connection.cursor()
+        sql="SELECT prueba FROM prueba"
+        cursor.execute(sql)
+        listas=cursor.fetchall()
+        #print(listas)
+        data['listas'] = listas
+        data['mensaje']='Exito'
+    except Exception as ex:
+        data['mensaje']='Error...'
+    return jsonify(data)
+
+
 
 
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
