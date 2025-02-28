8852defc94feb8a7539e7a987661b14f34d15d0e
Brayan Ocampo <mrx.source.mrx@gmail.com>
2025-02-28 14:41:28 -0500

    se eliminaron algun codigo

--- app.py
+++ app.py
@@ -11,21 +11,7 @@ from models.entities.User import User
 
 app = Flask(__name__)
 
-db = MySQL(app)
-
-
-# ---------------------------------------------------------------------------- #
-#                                  Peticiones                                  #
-# ---------------------------------------------------------------------------- #
-# @app.before_request
-# def before_request():
-#     print("Antes de la petición...")
-
-
-# @app.after_request
-# def after_request(response):
-#     print("Después de la petición")
-#     return response
+# db = MySQL(app)
 
 
 # ---------------------------------------------------------------------------- #
@@ -37,39 +23,6 @@ db = MySQL(app)
 # app.config['MYSQL_DB'] = 'Kliiker'
 
 # conexion = MySQL(app)
-
-
-@app.route("/")
-def index():
-    return redirect(
-        url_for("login")
-    )  # al ingresar al la raíz se redirige a la ruta login
-
-
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        # print(request.form['user'])
-        # print(request.form['pass'])
-        user = User(0, request.form["user"], request.form["pass"])
-        logged_user = ModelUser.login(db, user)
-        if logged_user != None:
-            if logged_user.password:
-                return redirect(url_for("home"))
-            else:
-                flash("Invalid...")
-                return render_template("/")
-        else:
-            flash("User no encontrado...")
-            return render_template("/")
-
-    else:
-        return render_template("login.html")
-
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
 
 
 if __name__ == "__main__":
